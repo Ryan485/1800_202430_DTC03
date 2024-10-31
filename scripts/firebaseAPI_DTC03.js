db097829076efa2483d774ebfba8232a24dc6987
Ryan485 <song976431@gmail.com>
2024-10-30 22:37:11 -0700

    Login page set up in progress

--- scripts/firebaseAPI_DTC03.js
+++ scripts/firebaseAPI_DTC03.js
@@ -1,14 +1,15 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+const firebaseConfig = {
+    apiKey: "AIzaSyBh8eghNtST_yZmmGK05JhYqOzSZJO32co",
+    authDomain: "dtc-03-trello-assignment.firebaseapp.com",
+    projectId: "dtc-03-trello-assignment",
+    storageBucket: "dtc-03-trello-assignment.appspot.com",
+    messagingSenderId: "527031161104",
+    appId: "1:527031161104:web:c90a1a736785f06f685595"
 };
+
 
 //--------------------------------------------
 // initialize the Firebase app
@@ -17,3 +18,4 @@ var firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const storage = firebase.storage();
+
