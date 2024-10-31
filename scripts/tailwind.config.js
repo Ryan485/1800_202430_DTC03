9c7bfbc378dd032a9bfdf59689f7d3a246043ee0
Richie Zhang <richiezhang@protonmail.com>
2024-10-30 22:54:10 -0700

    Create assignments page

--- scripts/tailwind.config.js
+++ scripts/tailwind.config.js
@@ -4,6 +4,9 @@ tailwind.config = {
             colors: {
                 'navbar': '#2b3035',
                 'navbar-hover': 'rgb(106, 104, 104)',
+                'assignment': '#d9d9d9',
+                'progress-done': '#524343',
+                'progress-not-done': '#9f8d8d',
             },
         },
         fontFamily: {
