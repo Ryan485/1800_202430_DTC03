9760ca74980540e1c85cbac89459915dae0c51b3
Richie Zhang <richiezhang@protonmail.com>
2024-11-06 10:12:58 -0800

    Implement persistant logging to prevent page refreshes from Live Server from clearing the logs.

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,21 @@ function loadSkeleton() {
     console.log($('#footer').load('./components/footer.html'))
 }
 loadSkeleton()
+
+function restoreLog() {
+    $("#log").html(localStorage.getItem("log"))
+}
+
+function log(data, json = false) {
+    var time = new Date();
+    console.log(data, time)
+    var html = `<p>[${time.toLocaleString()} ${window.location.pathname}] ${json ? JSON.stringify(data) : data}</p>`
+    $("#log").prepend(html)
+    localStorage.setItem("log", html + localStorage.getItem("log"))
+}
+
+function clearLog() {
+    $("#log").empty()
+    localStorage.setItem("log", "")
+    console.log("Log cleared")
+}
