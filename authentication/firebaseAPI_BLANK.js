d57c6d146102512cc598dc39d7ac82c05df7ea16
Richie Zhang <richiezhang@protonmail.com>
2024-11-06 09:03:04 -0800

    Gitignore Firebase API keys

--- authentication/firebaseAPI_BLANK.js
+++ authentication/firebaseAPI_BLANK.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 const firebaseConfig = {
-    apiKey: "AIzaSyBh8eghNtST_yZmmGK05JhYqOzSZJO32co",
-    authDomain: "dtc-03-trello-assignment.firebaseapp.com",
-    projectId: "dtc-03-trello-assignment",
-    storageBucket: "dtc-03-trello-assignment.appspot.com",
-    messagingSenderId: "527031161104",
-    appId: "1:527031161104:web:c90a1a736785f06f685595"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 
